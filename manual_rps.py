97f981fa082f202f7123aa9ea698fd03c88089ed
ASK_78425 <alasdair.s.kelly@outlook.com>
2024-08-22 19:21:47 +0100

    Adds the get_winner function to compare useer and computer input, and output an overall winner.

--- manual_rps.py
+++ manual_rps.py
@@ -9,14 +9,32 @@ def get_computer_choice():
     '''Randomly returns one of three options:
     Rock, Paper, or Scissors'''
     rps_list = ["Rock", "Paper", "Scissors"]
-    choice = rd.choice(rps_list)
-    return choice
+    computer_choice = rd.choice(rps_list)
+    return computer_choice
 
 #%%
 # Create get_user_choice
 def get_user_choice():
     '''Takes in and returns user input'''
     user_choice = input("Enter choice: ")
-    return user_choice
+    return user_choice.capitalize()
 
 #%%
+# Create get_winner function
+def get_winner(computer_choice, user_choice):
+    '''Takes two RPS choices (computer vs user) and compares to output the winner
+       Arguments:
+         computer_choice
+         user_choice'''
+    if computer_choice == user_choice:
+        print(f"Computer choice: {computer_choice}")
+        print(f"Your choice: {user_choice}")
+        print("It's a tie!")
+    elif (computer_choice[0] == "R" and user_choice[0] == "P") or (computer_choice[0] == "S" and user_choice[0] == "R") or (computer_choice[0] == "P" and user_choice[0] == "S"):
+        print(f"Computer choice: {computer_choice}")
+        print(f"Your choice: {user_choice}")
+        print("You won!")
+    else:
+        print(f"Computer choice: {computer_choice}")
+        print(f"Your choice: {user_choice}")
+        print("You lost")
