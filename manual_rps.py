046c8069d38e140b5ae333b9e9ba8ac20e030c38
ASK_78425 <alasdair.s.kelly@outlook.com>
2024-08-29 14:45:13 +0100

    Replaces user-typed input with model input. Tracks wins up to three rounds, and prints the winner.

--- manual_rps.py
+++ manual_rps.py
@@ -2,6 +2,9 @@
 #%%
 # Imports
 import random as rd
+from keras.models import load_model
+from camera_rps import get_prediction
+from camera_rps import countdown_from_three
 
 #%%
 # Create get_computer_choice
@@ -15,17 +18,27 @@ def get_computer_choice():
 #%%
 # Create get_user_choice
 def get_user_choice():
-    '''Takes in and returns user input'''
-    user_choice = input("Enter choice: ")
-    return user_choice.capitalize()
+    prediction_index = get_prediction()
+    user_choice = []
+    if prediction_index == 0:
+        user_choice = "Rock"
+    elif prediction_index == 1:
+        user_choice = "Paper"
+    elif prediction_index == 2:
+        user_choice = "Scissors"
+    else:
+        user_choice = "Nothing"
+    return user_choice
+
 
 #%%
 # Create get_winner function
 def get_winner(computer_choice, user_choice):
-    '''Takes two RPS choices (computer vs user) and compares to output the winner
+    '''Takes two RPS choices (computer vs user) and compares to output the winner. Calculates winner out of 3 games.
        Arguments:
          computer_choice
          user_choice'''
+    winner_key = 0
     if computer_choice == user_choice:
         print(f"Computer choice: {computer_choice}")
         print(f"Your choice: {user_choice}")
@@ -34,15 +47,34 @@ def get_winner(computer_choice, user_choice):
         print(f"Computer choice: {computer_choice}")
         print(f"Your choice: {user_choice}")
         print("You won!")
+        winner_key += 1
     else:
         print(f"Computer choice: {computer_choice}")
         print(f"Your choice: {user_choice}")
         print("You lost")
+        winner_key +=2
+    return winner_key
 
 #%%
-# Create function (play()) to simulate the game, amalgamating all prior functions
+# Create function play() to simulate the game, amalgamating all prior functions
 def play():
-    '''This function runs a game of RPS and displays the winner (computer or user)'''
-    play_user_choice = get_user_choice()
-    play_computer_choice = get_computer_choice()
-    get_winner(play_computer_choice, play_user_choice)
+    '''This function runs 3 games of RPS and displays
+    the winner (computer or user)'''
+    computer_wins = 0
+    user_wins = 0
+    while computer_wins < 3 and user_wins < 3:
+        play_user_choice = get_user_choice()
+        play_computer_choice = get_computer_choice()
+        result = get_winner(play_computer_choice, play_user_choice)
+        if result == 1:
+            user_wins += 1
+        elif result == 2:
+            computer_wins += 1
+        else:
+            continue
+    else:
+        print("Rounds completed")
+    if computer_wins > user_wins:
+        print("Computer has won three rounds, you lose.")
+    else: 
+        print("You have won three rounds! You win!")
